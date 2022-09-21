8ad31f134f093cf1780966b315e3b685789460f4
EricSoklowski <ericsoko2002@gmail.com>
2022-09-21 00:33:30 -0400

    complete lab

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Eric Sokolowski', done: true, _id: 125223},
+  {text: 'Soko Ericowski', done: false, _id: 127904},
+  {text: 'Eric Matthew Sokolowski', cool: false, _id: 139608},
+]
+
+export { 
+	students
+}
